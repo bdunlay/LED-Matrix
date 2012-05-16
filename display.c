e61aefc458d8232a806c247c1ae4f2b2ecfc0072
Brian Dunlay <brian.dunlay@gmail.com>
2012-05-16 11:35:19 -0700

    rewrote test to utilize printd

--- display.c
+++ display.c
@@ -12,34 +12,23 @@ byte currentData[8];
 void refresh() {
 	int i;
 	int select = 0x01;
+	
 	for (i = 0; i < 8; i++) {
-		
-		// write a row of 8 bits
-		writeShift(currentData[i], DISPLAY_WIDTH, DATA);
-		
-		// select that row
-		writeShift(select, 1, SELECT);
-		
-		// move the bit down the shift register
-		if (select) select = 0x00;
-
+		writeShift(currentData[i], DISPLAY_WIDTH, DATA); // write row of 8 bits
+		writeShift(select, 1, SELECT); // select row
+		if (select) select = 0x00; // push a 0 behind the select bit
 	}
 }
 
 void printd(byte data[]) {
 	int i = 0;
+
 	for (i = 0; i < 8; i++) {
 		currentData[i] = ~data[i]; // invert so that 0s will sink current
 	}
 }
 
 void test_display() {
-		currentData[0] = 0x24; //0 0 1 0 0 1 0 0
-		currentData[1] = 0x24; //0 0 1 0 0 1 0 0
-		currentData[2] = 0x24; //0 0 1 0 0 1 0 0
-		currentData[3] = 0x24; //0 0 1 0 0 1 0 0
-		currentData[4] = 0x24; //0 0 1 0 0 1 0 0
-		currentData[5] = 0x81; //1 0 0 0 0 0 0 1
-		currentData[6] = 0x42; //0 1 0 0 0 0 1 0
-		currentData[7] = 0x3C; //0 0 1 1 1 1 0 0
+	byte smiley[8] = {0x24, 0x24, 0x24, 0x24, 0x24, 0x81, 0x42, 0x3C};
+	printd(smiley);
 }
