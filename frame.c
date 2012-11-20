923607c01f4930048bf5947f404feafebdb783a0
bdunlay <brian.dunlay@gmail.com>
2012-11-19 23:37:44 -0800

    cleaning up code

--- frame.c
+++ frame.c
@@ -1,13 +1,10 @@
 /* Name: frame.c
  * Author: Brian Dunlay
  * Frame functions for LED Matrix for the ATtiny2313a
- * Some code borrowed / reworked from http://bildr.org/2011/02/74hc595/
  */
  
 #include "frame.h"
 
-
-//char frame[] = {0x00, 0x04, 0x42, 0x02, 0x02, 0x42, 0x04, 0x00};
 char frame[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 char* getFrame() 
