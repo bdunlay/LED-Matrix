1c64038de102c9d32e4b06ef2ba058c618298e18
wrmoy <william.r.moy@gmail.com>
2013-04-17 08:25:02 -0700

    added option for 16-bit timer

---
+++ timer16.h
@@ -0,0 +1,23 @@
+/*
+ * timer16.h
+ * 16-bit timer header file
+ * by William Moy
+ */
+
+#ifndef TIMER16_H
+#define TIMER16_H
+
+#include "timer_common.h"
+#include "types.h"
+
+void timer16_init(DWORD countTo, BYTE clockSelect);
+void timer16_start(void);
+void timer16_stop(void);
+BYTE timer16_is_tripped(void);
+BYTE timer16_is_running(void);
+void timer16_clear_flag(void);
+DWORD timer16_get_count(void);
+void timer16_enable_int(void);
+void timer16_disable_int(void);
+
+#endif
